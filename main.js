3fb666519ddec800b88dc08625c4106e107b8173
Queen-sanu <prarthanashashini93@gmail.com>
2025-07-05 09:18:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -53,8 +53,8 @@ async function downloadSessionData() {
         console.error('🛠️⚙️Please add your session to SESSION_ID env ‼️');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("Red_Fox-MD:/")[1];
-    const url = `https://pastebin.com/raw/${sessdata}`;
+    const sessdata = config.SESSION_ID.split("DORA-MD=")[1];
+    const url = `https://mega.nz/file/${sessdata}`;
     try {
         const response = await axios.get(url);
         const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
